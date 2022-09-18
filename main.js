28e0a53388c7b6906fc63d96d05619162956d378
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-09-18 21:33:11 +0000

    modified:   index.html 	modified:   main.js 	modified:   styles.css

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
+const setAlarmBtn = document.getElementById('setAlarm');
+const clock = document.querySelector(".clock");
+const alarm = document.querySelector(".alarm");
+const alarmTime = document.getElementById('timeSet');
 
-
+let alarmSet = false;
+let endDate = new Date();
 let startDate = new Date();
-let timeStr = 'The time is '+startDate.getHours+':'+startDate.getMinutes+':'+startDate.getSeconds;
+
+setAlarmBtn.addEventListener('click', () => {
+   let alarmStr = 'Timer will go off at: ';
+   alarmSet = true;
+   endDate = new Date();
+   if(alarmTime.value === ''){
+    endDate.setSeconds(endDate.getSeconds() + 10);
+   } else {
+    endDate.setSeconds(endDate.getSeconds() + Number(alarmTime.value));
+   }
+   alarmStr += formatTime(endDate);
+   alarm.textContent = alarmStr;
+   
+});
+
+function checkTime(){
+    if(alarmSet && endDate.getSeconds() === startDate.getSeconds()){
+        alert('Time has been reached');
+        
+    }
+}
 
 function updateTime(){
-    let newDate = new Date();
-    
+    let timeStr = 'The time is ';
+    startDate = new Date();
+    timeStr += formatTime(startDate);
+    clock.textContent = timeStr;
+    checkTime();
 }
+
+function formatTime(date){
+    let timeStr = '';
+    if(date.getHours() > 12 ){
+        date.setHours(date.getHours() % 12);
+        timeStr += '0'+date.getHours()+":";
+    }
+    if(date.getMinutes() < 10)
+        timeStr += '0';
+    timeStr += date.getMinutes()+':';
+    if(date.getSeconds() < 10)
+        timeStr += '0';
+    timeStr += date.getSeconds();
+    return timeStr;
+}
+
+setInterval(updateTime, 100);
